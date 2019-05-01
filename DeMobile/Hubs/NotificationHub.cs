cb5b6a9b7098efe4ac51e0af6299c21bda8c7d2b
chaiprapun <chaiprapun@hotmail.com>
2019-05-01 16:28:18 +0700

    - register context notification hub

--- DeMobile/Hubs/NotificationHub.cs
+++ DeMobile/Hubs/NotificationHub.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using DeMobile.Concrete;
+using DeMobile.Services;
 using Microsoft.AspNet.SignalR;
+using Oracle.ManagedDataAccess.Client;
 
 namespace DeMobile.Hubs
 {
     public class NotificationHub : Hub
     {
+        private User user;
         public override Task OnConnected()
         {
             string connectId = Context.ConnectionId;
@@ -23,6 +27,50 @@ namespace DeMobile.Hubs
         {
             var context = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
             context.Clients.All.notify(message);
+        }
+        public void registerContext(string device_id)
+        {
+            user = new User();
+            var connectId = Context.ConnectionId;
+            try
+            {
+                var device = user.checkCurrentDevice(device_id);
+                if (device != null && device.device_status == "ACT")
+                {
+                    using (OracleConnection conn = new OracleConnection(Database.conString))
+                    {
+                        try
+                        {
+                            conn.Open();
+                            using (var cmd = new OracleCommand(SqlCmd.Notification.updateConnectId, conn) { CommandType = System.Data.CommandType.Text })
+                            {
+                                cmd.Parameters.Add(new OracleParameter("conn_id", connectId));
+                                cmd.Parameters.Add(new OracleParameter("device_id", device_id));
+                                cmd.ExecuteNonQueryAsync();
+                                cmd.Dispose();
+                                //var context = GlobalHost.ConnectionManager.GetHubContext<MonitorHub>();
+                                //context.Clients.All.Monitor(new { ConnectionId = Context.ConnectionId, DeviceId = device_id, Message = "Updated", Time = DateTime.Now.ToString("dd/MM/yyyy") + " " + DateTime.Now.ToShortTimeString() });
+                                //context.Clients.All.UserOnline(Context.ConnectionId);
+                            }
+                        }
+                        finally
+                        {
+                            conn.Close();
+                            conn.Dispose();
+                        }
+                    }
+                }
+                else
+                {
+                    var context = GlobalHost.ConnectionManager.GetHubContext<MonitorHub>();
+                    context.Clients.All.Monitor(new { ConnectionId = Context.ConnectionId, DeviceId = device_id, Message = "Not found device!", Time = DateTime.Now.ToString("dd/MM/yyyy") + " " + DateTime.Now.ToShortTimeString() });
+                }
+            }
+            catch (Exception e)
+            {
+                var context = GlobalHost.ConnectionManager.GetHubContext<MonitorHub>();
+                context.Clients.All.Monitor(new { ConnectionId = Context.ConnectionId, DeviceId = device_id, Message = e.Message, Time = DateTime.Now.ToString("dd/MM/yyyy") + " " + DateTime.Now.ToShortTimeString() });
+            }
         }
     }
 }
